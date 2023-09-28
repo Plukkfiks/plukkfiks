6f41c15136d782f7ae5a91fecef2258298fc83f9
Plukkfiks <146321712+Plukkfiks@users.noreply.github.com>
2023-09-28 13:01:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const startupAnimation = document.querySelector('.startup-animation');
-    const startupSound = new Audio('startup-sound.mp3'); // Endre til din lydfilssti
-
-    // Spill oppstartslyden
-    startupSound.oncanplaythrough = () => {
-        startupSound.play();
-    };
-
-    // Start oppstartsanimasjonen
-    setTimeout(() => {
-        startupAnimation.style.animation = 'custom-startup-animation 5s ease forwards';
-        startupAnimation.addEventListener('animationend', () => {
-            startupAnimation.style.display = 'none'; // Skjul oppstartsanimasjonen
-            // Pause lyden etter at animasjonen er ferdig
-            startupSound.pause();
-        });
-    }, 100); // Endre til riktig varighet for din animasjon
-});
 function redirectToVipps() {
     const vippSum = 80;  // Fast pris på 80 kr
     const vippNummer = 'DITT_VIPPS_NUMMER';  // Ditt Vipps-nummer
@@ -24,4 +5,3 @@ function redirectToVipps() {
     const vippURL = `vipps://vipp?amount=${vippSum}&receiverId=${vippNummer}&message=Opptenningsved`;
     window.location.href = vippURL;
 }
-
