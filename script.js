9e1d2d17a33cb59398f478b22c66e55b4687762f
Plukkfiks <146321712+Plukkfiks@users.noreply.github.com>
2023-09-28 13:07:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startupAnimation = document.querySelector('.startup-animation');
+    startupAnimation.style.display = 'none';  // Skjul oppstartsanimasjonen
+});
+
 function redirectToVipps() {
     const vippSum = 80;  // Fast pris på 80 kr
-    const vippNummer = 'DITT_VIPPS_NUMMER';  // Ditt Vipps-nummer
+    const vippNummer = '94193690';  // Ditt Vipps-nummer
 
     const vippURL = `vipps://vipp?amount=${vippSum}&receiverId=${vippNummer}&message=Opptenningsved`;
     window.location.href = vippURL;
